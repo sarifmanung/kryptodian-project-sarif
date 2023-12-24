ab9a25ea7f9b9622c386bcf0f363eac918996270
sarifmanung <406168026@yru.ac.th>
2023-12-24 20:05:00 +0700

    update init.sql

--- server/init.sql
+++ server/init.sql
@@ -1,76 +1,41 @@
-DROP DATABASE IF EXISTS dcaweb_de;
-CREATE DATABASE dcaweb_de CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-use dcaweb_de;
+DROP DATABASE IF EXISTS kryptodian_app;
+CREATE DATABASE kryptodian_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use kryptodian_app;
 
-CREATE TABLE `preassessment` (
-    id int AUTO_INCREMENT,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- Create User table
+CREATE TABLE User (
+  UserID INT PRIMARY KEY,
+  Username VARCHAR(255) NOT NULL,
+  Password VARCHAR(255) NOT NULL,
+  Email VARCHAR(255) NOT NULL,
 
-    ip varchar(100),
-    country varchar(100),
-    reason varchar(255),
-    teethtype varchar(100),
-    name varchar(255),
-    email varchar(150),
-    phone varchar(100),
-    message text,
-    url text,
-
- 
-    PRIMARY KEY (id)
+  -- Add UNIQUE constraint
+  UNIQUE (Username),
+  UNIQUE (Email)
 );
 
-CREATE TABLE `preassessment_images` (
-    id int AUTO_INCREMENT,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+-- Create Cryptocurrency table
+CREATE TABLE Cryptocurrency (
+  CryptoID INT PRIMARY KEY,
+  Name VARCHAR(255) NOT NULL,
+  LogoURL VARCHAR(255) NOT NULL,
 
-    preassessmentid int,
-    fieldname varchar(150),
-    originalname varchar(255),
-    mimetype varchar(100),
-    awskey varchar(150),
-    awslocation varchar(255),
-
-
-    PRIMARY KEY (id)
+  -- Add UNIQUE constraint
+  UNIQUE (Name)
 );
 
+-- Create Portfolio table
+CREATE TABLE Portfolio (
+  PortfolioID INT PRIMARY KEY,
+  UserID INT,
+  CryptoID INT,
+  Quantity DECIMAL(18, 2) NOT NULL,
+  PurchasePrice DECIMAL(18, 2) NOT NULL,
 
-CREATE TABLE  `partner_apply`(
-    id int AUTO_INCREMENT,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (UserID) REFERENCES User(UserID),
+  FOREIGN KEY (CryptoID) REFERENCES Cryptocurrency(CryptoID),
 
-name varchar(255),
-clinic_name varchar(255),
-email varchar(255),
-contact_name varchar(255),
-speciallty varchar(255),
-preferred_contact_method varchar(255),
-state varchar(255),
-area varchar(255),
-select_1 varchar(10), /* Do you have any experience in prescribing clear aligners? */
-select_2 varchar (10),/* if No, are you open to prescribing clear aligners for your patients in future? */
-year_experience int(100),
-tell_us_why varchar(255),
-
-PRIMARY KEY (id)
-
+  -- Add additional constraints
+  CHECK (Quantity >= 0),
+  CHECK (PurchasePrice >= 0)
 );
-
-CREATE TABLE `referral` (
-    id int AUTO_INCREMENT,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    name varchar(255),
-    phone varchar(100),
-    kolname varchar(255),
-
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE `referral`
-ADD firstname varchar(255);
-
-ALTER TABLE `referral`
-ADD lastname varchar(255);
-
