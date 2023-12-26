d1a1680297bf96cceff933d19be83683b3517f87
sarifmanung <406168026@yru.ac.th>
2023-12-26 21:23:59 +0700

    backend updated

--- server/routes/v1.js
+++ server/routes/v1.js
@@ -34,44 +34,102 @@ app.get("/", (req, res) => {
   res.send("api la");
 });
 
-app.get("/data", (req, res) => {
-  res.json({ message: "Hello from the backend! 333" });
-});
-
-// post form (react)
-app.post("/register", async (req, res) => {
-  let { username, email, phone, password } = req.body;
+// post create new user
+app.post("/createUser", async (req, res) => {
+  const { username, email, phone, password } = req.body;
 
   // hash password
-  let hash_password = await hashPassword(password);
+  const hash_password = await hashPassword(password);
 
   // Check if the username already exists
   const checkUsernameQuery = "SELECT * FROM Users WHERE username = ?";
-  pool.query(checkUsernameQuery, [username], async function (err, results) {
-    if (err) {
-      console.error(err);
-      return res.status(404).send("MySql error!");
-    }
-
-    // If username already exists, return an error response
-    if (results.length > 0) {
-      return res.status(400).send("Username already exists");
-    }
-
-    // If username doesn't exist, proceed to insert the new user
-    const insertUserQuery =
-      "INSERT INTO Users (username, email, phone, password) VALUES (?, ?, ?, ?)";
-    const insertData = [username, email, phone, hash_password];
-    pool.query(insertUserQuery, insertData, async function (err, endresult) {
+  pool.query(
+    checkUsernameQuery,
+    [username],
+    async function (err, usernameResults) {
       if (err) {
         console.error(err);
-        return res.status(404).send("MySql error!");
+        return res.status(500).json({ error: "MySql error!" });
       }
-      req.session.mysqlid = endresult.insertId;
-      // send fast response and let everything do slowly
+
+      // If username already exists, return an error response
+      if (usernameResults.length > 0) {
+        console.log("Username already exists");
+        return res.status(400).json({ error: "Username already exists" });
+      }
+
+      // Check if the email already exists
+      const checkEmailQuery = "SELECT * FROM Users WHERE email = ?";
+      pool.query(checkEmailQuery, [email], async function (err, emailResults) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "MySql error!" });
+        }
+
+        // If email already exists, return an error response
+        if (emailResults.length > 0) {
+          console.log("Email already exists");
+          return res.status(400).json({ error: "Email already exists" });
+        }
+
+        // If username and email don't exist, proceed to insert the new user
+        const insertUserQuery =
+          "INSERT INTO Users (username, email, phone, password) VALUES (?, ?, ?, ?)";
+        const insertData = [username, email, phone, hash_password];
+        pool.query(
+          insertUserQuery,
+          insertData,
+          async function (err, endresult) {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({ error: "MySql error!" });
+            }
+            req.session.mysqlid = endresult.insertId;
+            // send a success response
+            res.json({ message: "User created successfully" });
+          }
+        );
+      });
+    }
+  );
+});
+
+// /auth/login
+app.post("/auth/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email exists in the database
+    const userQuery = "SELECT * FROM Users WHERE email = ?";
+    pool.query(userQuery, [email], async function (err, results) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "MySql error!" });
+      }
+
+      // If the email does not exist, return an error response
+      if (results.length === 0) {
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+
+      // Check if the password is correct
+      const storedPassword = results[0].password; // Assuming password is stored securely (hashed) in the database
+
+      // Use a dedicated function to compare passwords
+      const passwordMatch = await comparePassword(password, storedPassword);
+
+      if (!passwordMatch) {
+        // Use a generic error message to avoid revealing too much information
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+
+      console.log("login auth correctly~");
       res.send("ok");
     });
-  });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 module.exports = app;
