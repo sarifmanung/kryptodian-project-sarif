1a00ed6d1befd3f5107ade32c0b1571345a8d925
sarifmanung <406168026@yru.ac.th>
2023-12-26 15:39:10 +0700

    update init.sql

--- server/init.sql
+++ server/init.sql
@@ -3,20 +3,20 @@ CREATE DATABASE kryptodian_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 use kryptodian_app;
 
 -- Create User table
-CREATE TABLE `User` (
-  UserID INT PRIMARY KEY,
-  Username VARCHAR(255) NOT NULL,
-  Password VARCHAR(255) NOT NULL,
-  Email VARCHAR(255) NOT NULL,
+CREATE TABLE `Users` (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone varchar(100),
 
   -- Add UNIQUE constraint
-  UNIQUE (Username),
-  UNIQUE (Email)
+  UNIQUE (username)
 );
 
 -- Create Cryptocurrency table
 CREATE TABLE `Cryptocurrency` (
-  CryptoID INT PRIMARY KEY,
+  crypto_id INT AUTO_INCREMENT PRIMARY KEY,
   Name VARCHAR(255) NOT NULL,
   LogoURL VARCHAR(255) NOT NULL,
 
@@ -26,16 +26,42 @@ CREATE TABLE `Cryptocurrency` (
 
 -- Create Portfolio table
 CREATE TABLE `Portfolio` (
-  PortfolioID INT PRIMARY KEY,
-  UserID INT,
-  CryptoID INT,
-  Quantity DECIMAL(18, 2) NOT NULL,
-  PurchasePrice DECIMAL(18, 2) NOT NULL,
-
-  FOREIGN KEY (UserID) REFERENCES User(UserID),
-  FOREIGN KEY (CryptoID) REFERENCES Cryptocurrency(CryptoID),
-
-  -- Add additional constraints
-  CHECK (Quantity >= 0),
-  CHECK (PurchasePrice >= 0)
+  `portfolio_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `user_id` INT,
+  `crypto_id` INT,
+  `quantity` DECIMAL(18, 2) NOT NULL,
+  `purchase_price` DECIMAL(18, 2) NOT NULL,
+  FOREIGN KEY (`user_id`) REFERENCES `Users`(`user_id`),
+  FOREIGN KEY (`crypto_id`) REFERENCES `Cryptocurrency`(`crypto_id`)
 );
+
+-- Create trigger for quantity constraint
+DELIMITER //
+
+CREATE TRIGGER check_portfolio_quantity
+BEFORE INSERT ON Portfolio
+FOR EACH ROW
+BEGIN
+   IF NEW.quantity < 0 THEN
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Quantity must be greater than or equal to 0';
+   END IF;
+END//
+
+DELIMITER ;
+
+-- Create trigger for purchase_price constraint
+DELIMITER //
+
+CREATE TRIGGER check_portfolio_purchase_price
+BEFORE INSERT ON Portfolio
+FOR EACH ROW
+BEGIN
+   IF NEW.purchase_price < 0 THEN
+      SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'Purchase price must be greater than or equal to 0';
+   END IF;
+END//
+
+DELIMITER ;
+
