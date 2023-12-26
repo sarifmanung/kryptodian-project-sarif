b55f4652022430cb727a33af3a671495ee6fcc06
sarifmanung <406168026@yru.ac.th>
2023-12-26 11:25:25 +0700

    updated

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter{" "}
+            CRYPTODIAN{" "}
           </Typography>{" "}
           <Typography
             variant="subtitle2"
